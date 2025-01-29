d0fafebbfd922528eab9c77f65d0b75ec28efa04
dalioshin <dalioshin@gmail.com>
2025-01-29 00:32:04 -0800

    update data manip to convert to cartesian

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 This is a fun project designed to explore the NASA exoplant datasets
 and hopefully create some art as well.
+
+
+USE of project
+Dependencies:
+blender
+
+Run the data manipulation
+python3 exo_planet/scale_transform_data.py PSCompPars_2024.04.08_23.19.41.csv
+
+Run a processig sketch example
+processing-java --sketch=pointfield --run
---
+++ exo_planet/scale_transform_data.py
@@ -0,0 +1,37 @@
+import sys
+import numpy as np
+import pandas as pd
+from data_func import load_from_csv
+
+def scale_dist(df) -> pd.DataFrame:
+    df["sy_dist"] = df["sy_dist"] / df["sy_dist"].max() * 100
+    return df
+
+def convert_to_cart(df) -> pd.DataFrame:
+    df["x"] = df["sy_dist"] * np.sin(df["dec"]) * np.cos(df["ra"])
+    df["y"]  = df["sy_dist"] * np.sin(df["dec"]) * np.sin(df["ra"])
+    df["z"]  = df["sy_dist"] * np.sin(df["dec"])
+    return df
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("provide a filename")
+        sys.exit(1)
+
+    df = load_from_csv(sys.argv[1])    
+
+    # pull spec columns from df and scale distance
+    df_location_only = df[["ra", "dec", "sy_dist", "pl_rade"]]
+    df_loc_scaled = scale_dist(df_location_only)
+    
+    # convert polar to cartesian coords
+    df_scaled_cart = convert_to_cart(df_loc_scaled)
+    
+    # output new csv for blender consumption
+    df_scaled_cart.dropna(inplace=True)
+    df_scaled_cart.to_csv('scaled_cart_data.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
